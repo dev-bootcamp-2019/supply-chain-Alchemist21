47dd97da3c378e7e73a48b0b86632aa1049a8e2b
Louell Sala <louell_sala@yahoo.com>
2018-12-01 21:34:11 -0500

    Version1

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -6,11 +6,11 @@ contract SupplyChain {
   address owner;
 
   /* Add a variable called skuCount to track the most recent sku # */
-
+  uint128 public skuCount;
   /* Add a line that creates a public mapping that maps the SKU (a number) to an Item.
      Call this mappings items
   */
-
+  mapping (uint => Item) public items;
   /* Add a line that creates an enum called State. This should have 4 states
     ForSale
     Sold
@@ -18,17 +18,31 @@ contract SupplyChain {
     Received
     (declaring them in this order is important for testing)
   */
+  enum State { ForSale, Sold, Shipped, Received}
 
   /* Create a struct named Item.
     Here, add a name, sku, price, state, seller, and buyer
     We've left you to figure out what the appropriate types are,
     if you need help you can ask around :)
   */
+  struct Item {
+    uint128 sku;
+    uint128 price;
+    string name;
+    State name;
+    address seller;
+    address buyer;
+  }
 
   /* Create 4 events with the same name as each possible State (see above)
     Each event should accept one argument, the sku*/
+    event ForSale(uint sku);
+    event Sold(uint sku);
+    event Shipped(uint sku);
+    event Received(uint sku);
 
 /* Create a modifer that checks if the msg.sender is the owner of the contract */
+    require (msg.sender == owner, "only the owner can access this")
 
   modifier verifyCaller (address _address) { require (msg.sender == _address); _;}
 
@@ -44,15 +58,33 @@ contract SupplyChain {
   /* For each of the following modifiers, use what you learned about modifiers
    to give them functionality. For example, the forSale modifier should require
    that the item with the given sku has the state ForSale. */
-  modifier forSale
-  modifier sold
-  modifier shipped
-  modifier received
+  modifier forSale(uint_sku) {
+    require (items[sku].state) == State.Forsale, "this item is for sale");
+    _;
+  }
+
+  modifier sold(uint _sku) {
+    require (items[sku].state) == State.Sold, "this item is sold");
+    _;
+  }
+
+  modifier shipped(uint _sku) {
+      require (items[sku].state) == State.Shipped, "this item is shipped");
+    _;
+  }
+
+
+  modifier received(uint _sku) {
+      require (items[sku].state) == State.Received, "this item is received");
+      _;
+  }
 
 
   constructor() public {
     /* Here, set the owner as the person who instantiated the contract
        and set your skuCount to 0. */
+       owner = msg.sender;
+       skuCount = 0;
   }
 
   function addItem(string _name, uint _price) public returns(bool){
@@ -70,19 +102,52 @@ contract SupplyChain {
 
   function buyItem(uint sku)
     public
-  {}
+    payable
+    forSale(sku)
+    paidEnough(msg.value)
+    checkValue(sku)
+  {
+    //struct in storage
+    Item storage i = items[sku];
+    //change state
+    i.buyer = msg.sender;
+    i.state = State.Sold;
+    //transfer to seller
+    //emit events
+    emit Sold(sku);
+  }
 
   /* Add 2 modifiers to check if the item is sold already, and that the person calling this function
   is the seller. Change the state of the item to shipped. Remember to call the event associated with this function!*/
   function shipItem(uint sku)
     public
-  {}
+    sold(sku)
+    verifyCaller(items[sku].seller)
+  {
+    //struct in storage
+    Item storage i = items[sku];
+    //change state
+    i.buyer = msg.sender;
+    i.state = State.Shipped;
+    //transfer to seller
+    //emit events
+    emit Shipped(sku);
+  }
 
   /* Add 2 modifiers to check if the item is shipped already, and that the person calling this function
   is the buyer. Change the state of the item to received. Remember to call the event associated with this function!*/
   function receiveItem(uint sku)
     public
-  {}
+    shipped(sku)
+    verifyCaller(items[sku].buyer)
+  {
+    //struct in storage
+    Item storage i = items[sku];
+    //change state
+    i.state = State.Received;
+    //emit events
+    emit Received(sku);
+  }
 
   /* We have these functions completed so we can run tests, just ignore it :) */
   function fetchItem(uint _sku) public view returns (string name, uint sku, uint price, uint state, address seller, address buyer) {
@@ -93,6 +158,11 @@ contract SupplyChain {
     seller = items[_sku].seller;
     buyer = items[_sku].buyer;
     return (name, sku, price, state, seller, buyer);
+  }
+  //function to test onlyOwner
+  function accessByOwner() public view onlyOwner() returns(bool){
+    //will only return when modifier conditions are met
+    return true;
   }
 
 }
--- test/TestSupplyChain.sol
+++ test/TestSupplyChain.sol
@@ -8,24 +8,118 @@ contract TestSupplyChain {
 
     // Test for failing conditions in this contracts
     // test that every modifier is working
-
+    function testOnlyOwnerModifier() public {
+      //instantiate contract
+      SupplyChain s = new SupplyChain();
+      ProxyTester p = nre ProxyTester();
+      //declare value
+      uint expecter = 1;
+      //test assertion
+     Assert.equal(s.accessByOwner(), expected, "msg.sender should be the owner");
+     //call via proxy; should return false
+     bool proxyCallResult = p.accessByOwnerProxy(s);
+     Assert.isFalse(proxyCallResult, "accessByOwner() should throw an exception when called by non-owner");
+    }
     // buyItem
-
     // test for failure if user does not send enough funds
+    function testItemPurchaseWithInsufficientFunds() public {
+    //instantiate
+    SupplyChain s = new SupplyChain();
+    //add item
+    s.addItem("first item", 100);
+    //attempt to call buy item
+    bytes memory functionSignature = padFunctionWithOneByteArgument("buyItem(uint256)", 0x00);
+    bool attemptedBuyCall = address(s).call.value(99)(functionSignature);
+    //check assertion
+    Assert.isFalse(attemptedBuyCall, "buyItem() should throw an exception");
+  }
+
+
     // test for purchasing an item that is not for Sale
+    function testItemPurchaseWhenNotForSale() public {
+      //instantiate contracts
+      SupplyChain s = new SupplyChain();
+      ProxyTester p = new ProxyTester();
+      //add item
+      s.addItem("first item", 100);
+      //buy via proxy
+      p.buyProxy.value(100)(s);
+      //attempt to call buy item on same item
+      bytes memory functionSignature = padFunctionWithOneByteArgument("buyItem(uint256)", 0x00);
+      bool attemptedBuyCall = address(s).call.value(100)(functionSignature);
+      //check assertion
+      Assert.isFalse(attemptedBuyCall, "buyItem() should throw an exception");
+    }
+
 
 
     // shipItem
-
     // test for calls that are made by not the seller
+    function testShipItemNotFromSeller() public {
+    //instantiate
+    SupplyChain s = new SupplyChain();
+    //add item
+    s.addItem("first item", 100);
+    //establish expected address (reconstructed tuple required here)
+    (,,,,address expected,) = s.fetchItem(0);
+    //check assertion
+    Assert.equal(address(this), expected, "msg.sender should be the sender");
+    }
+
+
+
+
     // test for trying to ship an item that is not marked Sold
+    function testShipItemNotYetSold() public {
+      //instantiate
+      SupplyChain s = new SupplyChain();
+      //add item
+      s.addItem("first item", 100);
+      //attempt to ship item
+      bytes memory functionSignature = padFunctionWithOneByteArgument("shipItem(uint256)", 0x00);
+      bool attemptedShipCall = address(s).call(functionSignature);
+      //establish expected value
+      (,,,uint expected,,) = s.fetchItem(0); //expected state == 0 == State.ForSale
+      //check assertions
+      Assert.isFalse(attemptedShipCall, "shipItem() should throw an exception");
+      Assert.equal(0, expected, "the state should be ForSale");
+     }
+
 
     // receiveItem
-
     // test calling the function from an address that is not the buyer
+    function testReceiveItemNotFromBuyer() public {
+    //instantiate contracts
+    SupplyChain s = new SupplyChain();
+    ProxyTester p = new ProxyTester();
+    //add an item
+    s.addItem("first item", 100);
+    //buy and ship item with ProxyTester
+    p.buyProxy.value(100)(s);
+    //ship item
+    s.shipItem(0);
+    //attempt to call receive item
+    bytes memory functionSignature = padFunctionWithOneByteArgument("receiveItem(uint256)", 0x00);
+    bool attemptedReceiveCall = address(s).call(functionSignature);
+    //check assertion
+    Assert.isFalse(attemptedReceiveCall, "receiveItem() should throw an exception");
+    }
+
+
+
     // test calling the function on an item not marked Shipped
 
-     
-
-
-}
+    function testReceiveItemNotYetShipped() public {
+   //instantiate
+   SupplyChain s = new SupplyChain();
+   ProxyTester p = new ProxyTester();
+   //add item via proxy
+   p.addProxy(s);
+   //buy item
+   s.buyItem.value(100)(0);
+   //attempt to call receive item
+   bytes memory functionSignature = padFunctionWithOneByteArgument("receiveItem(uint256)", 0x00);
+   bool attemptedReceiveCall = address(s).call(functionSignature);
+   //check assertion
+   Assert.isFalse(attemptedReceiveCall, "receiveItem() should throw an exception");
+  }
